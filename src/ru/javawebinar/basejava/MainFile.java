9606acda761f1d99be5086ddf9a5b8ec741074d5
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-11-24 14:49:21 +0300

    HW09 Implement recursive filenames output with indents

--- src/ru/javawebinar/basejava/MainFile.java
+++ src/ru/javawebinar/basejava/MainFile.java
@@ -3,12 +3,25 @@ package ru.javawebinar.basejava;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * gkislin
  * 21.07.2016
  */
 public class MainFile {
+    private static final Comparator<File> filesAndDirComparator = (o1, o2) -> {
+        if (o1.isFile() && o2.isDirectory()) {
+            return 1;
+        }
+        if (o1.isDirectory() && o2.isFile()) {
+            return -1;
+        }
+        return 0;
+    };
+
     public static void main(String[] args) {
         String filePath = ".\\.gitignore";
 
@@ -36,16 +49,20 @@ public class MainFile {
 
         System.out.println("\n\n======================================================");
         System.out.println("Рекурсивный вывод имен файлов:");
-        printFileNames(dir);
+        printFileNames(dir, "");
     }
 
-    private static void printFileNames(File file) {
+    private static void printFileNames(File file, String indent) {
         if (file.isFile()) {
-            System.out.println(file.getName());
+            System.out.println(indent + file.getName());
         } else if (file.isDirectory()) {
+            System.out.println(indent + file.getName());
             File[] files = file.listFiles();
-            for (File f : files) {
-                printFileNames(f);
+            if (files != null) {
+                List<File> list = Arrays.stream(files).sorted(filesAndDirComparator).toList();
+                for (File f : list) {
+                    printFileNames(f, indent + "  ");
+                }
             }
         }
     }
