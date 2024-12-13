b97968cd300d59e143b9e981a6641145a5ca6f48
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-12-13 18:00:34 +0300

    Lesson12 Concurrency

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,13 +1,27 @@
 package ru.javawebinar.basejava;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class MainConcurrency {
     public static final int THREADS_NUMBER = 10000;
     private int counter;
-    private static final Object LOCK = new Object();
-    private static final Object LOCK_2 = new Object();
+    private final AtomicInteger atomicCounter = new AtomicInteger();
+    // private static Object LOCK = new Object();
+    // private static final Lock lock = new ReentrantLock();
+    private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
+    private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
+    private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<>(){
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat();
+        }
+    };
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
@@ -16,7 +30,7 @@ public class MainConcurrency {
             @Override
             public void run() {
                 System.out.println(getName() + ", " + getState());
-                throw new IllegalStateException();
+                // throw new IllegalStateException();
             }
         };
         thread0.start();
@@ -39,32 +53,46 @@ public class MainConcurrency {
         System.out.println(thread0.getState());
 
         final MainConcurrency mainConcurrency = new MainConcurrency();
-        List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
+        CountDownLatch latch = new CountDownLatch(THREADS_NUMBER);
+        ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        // CompletionService completionService = new ExecutorCompletionService(executorService);
+        //
+        // List<Thread> threads = new ArrayList<>(THREADS_NUMBER);
 
-        for (int i = 0; i < THREADS_NUMBER; i++) {
-            Thread thread = new Thread(() -> {
-                for (int j = 0; j < 100; j++) {
-                    mainConcurrency.inc();
-                }
-            });
-            thread.start();
-            threads.add(thread);
+         for (int i = 0; i < THREADS_NUMBER; i++) {
+         //   Thread thread = new Thread(() -> {
+             Future<Integer> future = executorService.submit(() ->
+             {
+                 for (int j = 0; j < 100; j++) {
+                     mainConcurrency.inc();
+                     System.out.println(threadLocal.get().format(new Date()));
+                 }
+                 latch.countDown();
+                 return 5;
+             });
+        //    thread.start();
+        //    threads.add(thread);
         }
 
-        threads.forEach(t -> {
+        /*threads.forEach(t -> {
             try {
                 t.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         });
-        System.out.println(mainConcurrency.counter);
+        System.out.println(mainConcurrency.counter);*/
+
+        latch.await(10, TimeUnit.SECONDS);
+        executorService.shutdown();
+        System.out.println(mainConcurrency.atomicCounter.get());
     }
 
-    private synchronized void inc() {
+    private void inc() {
 //        synchronized (this) {
 //        synchronized (MainConcurrency.class) {
-        counter++;
+        atomicCounter.incrementAndGet();
+        // counter++;
 //                wait();
 //                readFile
 //                ...
