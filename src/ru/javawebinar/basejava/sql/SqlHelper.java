7787dd04636bf4ad2958479f13165d822bc18e26
krkat <98359411+EmmaFranBengtsbo@users.noreply.github.com>
2024-12-19 13:35:53 +0300

    Lesson14 JOIN

--- src/ru/javawebinar/basejava/sql/SqlHelper.java
+++ src/ru/javawebinar/basejava/sql/SqlHelper.java
@@ -1,9 +1,5 @@
 package ru.javawebinar.basejava.sql;
 
-import ru.javawebinar.basejava.exception.ExistStorageException;
-import ru.javawebinar.basejava.exception.StorageException;
-import ru.javawebinar.basejava.sql.ConnectionFactory;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -27,32 +23,4 @@ public class SqlHelper {
             throw ExceptionUtil.convertException(e);
         }
     }
-    /*public static void execute(ConnectionFactory factory, String statement, Executor executor) {
-        try (Connection conn = factory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(statement)) {
-            executor.execute(ps);
-        } catch (SQLException e) {
-            if ("23505". equals(e.getSQLState())) {
-                throw new ExistStorageException(e.getMessage());
-            }
-            throw new StorageException(e);
-        }
-    }
-
-    public static <T> T resultExecute(ConnectionFactory factory, String statement, ResultExecutor<T> executor) {
-        try (Connection conn = factory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(statement)) {
-            return executor.execute(ps);
-        } catch (SQLException e) {
-            throw new StorageException(e);
-        }
-    }
-
-    public interface Executor {
-        void execute(PreparedStatement ps) throws SQLException;
-    }
-
-    public interface ResultExecutor<T> {
-        T execute(PreparedStatement ps) throws SQLException;
-    }*/
 }
